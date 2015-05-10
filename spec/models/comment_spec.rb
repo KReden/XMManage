3090a3dcd861f958370a36baad56a1a26a04ad33
Kyle Reden <kreden@pathf.com>
2015-05-09 20:19:46 -0500

    fix

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,11 +4,11 @@ describe Comment, type: :model do
   context "Validations" do
     it "should validate the presence of user" do
       comment = FactoryGirl.build(:comment, user: nil)
-      expect(comment).to be_valid
+      expect(comment).to be_invalid
     end
     it "should validate the presence of story" do
       comment = FactoryGirl.build(:comment, story: nil)
-      expect(comment).to be_valid
+      expect(comment).to be_invalid
     end
   end
 end
